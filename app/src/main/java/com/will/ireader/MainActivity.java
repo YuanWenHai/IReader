d01f4852765641b40850609e3f9d272182282a38
YuanWenHai <a2265839@gmail.com>
2019-05-06 21:19:17 +0800

    修改了support lib version以及相应sdk版本，将在未来一段时间内重新完善应用

--- app/src/main/java/com/will/ireader/MainActivity.java
+++ app/src/main/java/com/will/ireader/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
