d25742f90d6745b5a17df84d819d358fc5d6f8db
ywh <a2265839@gmail.com>
2019-07-10 17:59:47 +0800

    2019/7/10 daily

--- app/src/main/java/com/will/ireader/printer/Printer.java
+++ app/src/main/java/com/will/ireader/printer/Printer.java
@@ -1,18 +1,22 @@
 package com.will.ireader.printer;
 
 import android.graphics.Paint;
-import android.support.annotation.Nullable;
 import android.util.Log;
 
 
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Printer {
 
     private final Book book;
 
-    private int startPos;
+    //当下，在文件bytes中的指针位置
+    private int currentPos;
+
+    private int pageStartPos;
     private long endPos;
 
 
@@ -25,6 +29,7 @@ public class Printer {
 
 
     public String[] printPageForward(int availableWidth,int availableHeight,Paint paint){
+        pageStartPos = currentPos;
         int rowCount = (int)(availableHeight/paint.getTextSize());
         String[] pageContent = new String[rowCount];
         for(int i=0;i<pageContent.length;i++){
@@ -42,24 +47,26 @@ public class Printer {
 
 
     private String printLineForward(int availableWidth, Paint paint){
-        if(startPos >= book.bytes().capacity()-1){
+        if(currentPos >= book.bytes().capacity()){
             return "";
         }
-        int lineEnd = startPos;
-        byte[] bytes;
-        String line = "";
-        for(;;){
-                if((lineEnd != startPos && book.bytes().get(lineEnd) == 10) || lineEnd == book.bytes().capacity()-1){
-                int length = lineEnd - startPos;
-                bytes = new byte[length];
-                for(int i=0;i<length;i++){
-                    bytes[i] = book.bytes().get(startPos+i);
-                }
-                startPos = lineEnd;
-                break;
+
+        int pEnd = currentPos;
+        while (pEnd < book.bytes().capacity()){
+                if(book.bytes().get(pEnd) == 0x0a){
+                    pEnd += 1; //将换行符(0x0a)一同加入段落,pEnd即currentPos总是指向下一段段首
+                    break;
             }
-            lineEnd++;
+            pEnd++;
         }
+        int length = pEnd - currentPos;
+        byte[] bytes = new byte[length];
+        for(int i=0;i<length;i++){
+            bytes[i] = book.bytes().get(currentPos +i);
+        }
+        currentPos = pEnd;
+
+        String line = "";
         try{
             String paragraph = new String(bytes,Charset.forName(book.getCharset()));
             int textCount = paint.breakText(paragraph,true,availableWidth,null);
@@ -67,7 +74,7 @@ public class Printer {
             String remain = paragraph.substring(textCount);
             if(remain.length() > 0){
                 int returnedIndex = remain.getBytes(book.getCharset()).length;
-                startPos -= returnedIndex;
+                currentPos -= returnedIndex;
             }
         }catch (UnsupportedEncodingException u){
             u.printStackTrace();
@@ -77,41 +84,54 @@ public class Printer {
     }
 
 
-    // TODO: 2019/7/9  考虑不足一页时的问题
     private void movePosToPreviousPage(int availableWidth,int availableHeight,Paint paint){
+        if(currentPos <= 0){
+            currentPos = 0;
+            return;
+        }
 
         int rowCount = (int)(availableHeight/paint.getTextSize());
-        for(int a=0;a<rowCount;a++){
-            int lineEnd = startPos;
-            byte[] bytes;
-            for(;;){
-                if((book.bytes().get(lineEnd) == 10 && lineEnd != startPos) || startPos == 0){
-                    int length = startPos - lineEnd;
-                    bytes = new byte[length];
-                    for(int i=0;i<length;i++){
-                        bytes[i] = book.bytes().get(lineEnd+i);
-                    }
-                    startPos = lineEnd;
+        ArrayList<String> lines = new ArrayList<>();
+        while(lines.size() < rowCount){
+
+            int pEnd = currentPos;
+            while (pEnd > 0){
+                if((book.bytes().get(pEnd) == 10 && currentPos-pEnd !=1)  || currentPos == 0){
+                    pEnd += 1;//指针后移一位，不将上一段换行符加入本段
                     break;
                 }
-                lineEnd--;
+                pEnd--;
             }
+            int length = currentPos - pEnd;
+            byte[] bytes = new byte[length];
+            for(int i=0;i<length;i++){
+                bytes[i] = book.bytes().get(pEnd+i);
+            }
+            currentPos = pEnd;
+
+
             try{
                 String paragraph = new String(bytes,book.getCharset());
-                int textCount = paint.breakText(paragraph,true,availableWidth,null);
-                String line = paragraph.substring(0,textCount);
-                String remain = paragraph.substring(textCount);
-                if(remain.length() > 0){
-                    startPos += remain.getBytes(book.getCharset()).length;
+                List<String> temp = new ArrayList<>();
+                while(paragraph.length() > 0){
+                    int lineCount = paint.breakText(paragraph,true,availableWidth,null);
+                    temp.add(paragraph.substring(0,lineCount));
+                    paragraph = paragraph.substring(lineCount);
                 }
+                lines.addAll(0,temp);
+                if(lines.size() > rowCount){
+
+                }
+
+                if(paragraph.length() > 0){
+                    currentPos += paragraph.getBytes(book.getCharset()).length;
+                }
+
             }catch (UnsupportedEncodingException u){
                 u.printStackTrace();
                 Log.e("error on printer","unsupported charset");
             }
         }
-
-
-
     }
 
 
